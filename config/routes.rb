3679fd7f0a3316f02d8c08071a890b26012554a4
Seamus Narron <seamus@uga.edu>
2024-02-28 15:46:29 -0500

    Finish Crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  resources :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
