6711b782bd71ae76d1c3f4ed443bcba6a91607ab
Seamus Narron <seamus@uga.edu>
2024-02-28 13:59:37 -0500

    Add show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog_posts#index"
 end
